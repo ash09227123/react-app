8bb3c4c6d4da1015da6f68d8064d62620cb18db1
Paul He <Paul_He@epam.com>
2022-10-18 15:16:46 +0800

    feat:catelog function

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -2,7 +2,7 @@
  * @Author: Paul He Paul_He@epam.com
  * @Date: 2022-10-17 16:03:27
  * @LastEditors: Paul He Paul_He@epam.com
- * @LastEditTime: 2022-10-17 16:33:10
+ * @LastEditTime: 2022-10-18 15:14:42
  * @FilePath: \react-app\src\components\Catalog\index.jsx
  * @Description:
  *
@@ -13,18 +13,37 @@ import "./index.css";
 
 class Catalog extends PureComponent {
   state = {
-    current: 0,
+    cateTypes: [
+      { text: "ALL", checked: false },
+      { text: "DOCUMENTORY", checked: false },
+      { text: "COMEDY", checked: false },
+      { text: "HORROR", checked: false },
+      { text: "CRIME", checked: false },
+    ],
   };
+  switchCate(text) {
+    let mutatedCateTypes = this.state.cateTypes.map((item) => {
+      item.checked = item.text === text ? true : false;
+      return item;
+    });
+    this.setState({ cateTypes: mutatedCateTypes });
+  }
   render() {
     return (
       <>
         <div className="clasify_wrap">
           <div className="catalog">
-            <div>All</div>
-            <div>DOCUMENTORY</div>
-            <div>COMEDY</div>
-            <div>HORROR</div>
-            <div>CRIME</div>
+            {this.state.cateTypes.map((cate) => {
+              const { text, checked } = cate;
+              return (
+                <div
+                  onClick={() => (!checked ? this.switchCate(text) : false)}
+                  className={checked ? "selected" : ""}
+                >
+                  {text}
+                </div>
+              );
+            })}
           </div>
           <div className="sort_type">
             <div>SORT BY</div>
